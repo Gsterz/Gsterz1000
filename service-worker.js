15e6834a04aced934d9a2fe968ff413e542506b8
Gsterz <bbasz@naver.com>
2026-06-30 13:20:54 +0900

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-  '/Gsterz1000-1/',
-  '/Gsterz1000-1/index.html',
-  '/Gsterz1000-1/script.js',
-  '/Gsterz1000-1/manifest.json',
-  '/Gsterz1000-1/icon-192.jpg',
-  '/Gsterz1000-1/icon-512.jpg'
+  '/Gsterz1000/',
+  '/Gsterz1000/dashboard.html',
+  '/Gsterz1000/script.js',
+  '/Gsterz1000/manifest.json',
+  '/Gsterz1000/icon-192.jpg',
+  '/Gsterz1000/icon-512.jpg'
 ];
 
 self.addEventListener('install', event => {
